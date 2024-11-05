b56444158cac1fafb181ebdb0bfbbed0db7b4d2a
Atttttr <00001095646047sp@al.educacao.sp.gov.br>
2024-11-05 08:14:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual a formula do teorema de pitagoras?',
+    'Fórmula completa  a² + b² = c²'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual o comando que salva uma ação no VsCode?',
+    'Ctrl S'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual a formula da equação de torricelli?',
+    'A equação de Torricelli, no movimento uniformemente variado (MUV), é vf2=vi2+2∙a∙∆x . A equação de Torricelli, no movimento circular uniformemente variado (MCUV), é ωf2=ω02+2∙α∙∆φ .'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual o elemento mais abundande no universo?',
+    'Hidrogenio (H)'
+)
